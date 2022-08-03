237ace89459a0e5ce1cd19cc2bf9356f8dcb6f38
DiogoKahn <rm92928@fiap.com.br>
2022-08-03 09:21:23 -0300

    atualização

---
+++ rm92928/js/script.js
@@ -0,0 +1,28 @@
+// Comentário de 1 linha
+/**
+ * Comentário de bloco
+ */
+
+// Console ed saída
+console.log("Hello World!")
+
+// Document que escreve direto na página
+// document.write("Hello World!")
+
+// Variável global ex:
+// var nome
+// function alteraNome(nomeAlt){
+//     this.nome - nomeAlt;
+//     return nome
+// }
+
+// function verificacaoNome(nome){
+//     if(nome === undefined){
+//         nome = "vazio"
+//     }
+// }
+
+const btn = document.getElementById('btnEnviar')
+
+// Atrelando um evento
+// Escutar um evento
