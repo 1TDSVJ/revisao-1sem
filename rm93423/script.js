8bb4991130e135c5503ba4cac77a15c26d7e0716
jhonnyo3 <jhonatas2004@gmail.com>
2022-08-03 09:46:12 -0300

    arrow functions

--- rm93423/script.js
+++ rm93423/script.js
@@ -1,7 +1,10 @@
 
-let button = document.getElementById('btnEnviar')
-button.addEventListener('click', exibir)
+const btn = document.getElementById('btnEnviar')
+button.addEventListener('click', ()=>{
+    btn.type = 'button'
+    let inputTask = document.querySelector("imput[type=text]")
+    inputTask[0].value = btn.innerHTML
 
-function exibir() {
-    alert("Hello Horld")
-}
+})
+
+
