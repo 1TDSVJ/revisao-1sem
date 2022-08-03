a8fbc1ad14e95b908246e6c73151367f2483d7b6
MateusLem <rm93480@fiap.com.br>
2022-08-03 09:46:41 -0300

    js

--- rm93480/js/scrit.js
+++ rm93480/js/scrit.js
@@ -1,8 +1,43 @@
-//Comentário de 1 linha
+// <- Comentário de 1 linha
+
 /**
-*Comentário de bloco
+Comentário de bloco
 */
-//Console de saída
+
+// Console de saída
 console.log("Olá Pessoas")
-//Document que escreve direto na página
+
+// Document que escreve direto na página
 document.write("Olá Pessoas")
+
+/**
+
+var -> declara uma variável global
+
+let -> declara uma variável apenas dentro da função/ambiente específico
+
+const -> declara uma constante
+
+*/
+
+//1° Passo
+const btn = document.getElementById("btnEnviar")
+
+//Atrelando um evento
+//Escutar(Deixar um evento em aguardo) um evento e realizar uma ação
+/**
+    btn.addEventListener("click",function(){
+        //Mudou o tipo do botão
+        btn.type = "button"
+        //Recuperou o input nmTask querySelecor
+        let inputTask = document.querySelectorAll("input[type=text]")
+        //  ^ Criou um array com todos os inputs de tipo text
+        inputTask[0].value = btn.innerHTML
+        //           ^ Manda o valor| ^ para o campo
+    })
+*/
+btn.addEventListener("click",()=>{
+    btn.type = "button"
+    let inputTask = document.querySelectorAll("input[type=text]")
+    inputTask[0].value = btn.innerHTML
+})
