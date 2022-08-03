ac1d46f85e819a778b7e3189716b660657ef2ea4
albertthiago <atgs1717@gmail.com>
2022-08-03 10:09:25 -0300

    atualizado

--- rm96256/js/script.js
+++ rm96256/js/script.js
@@ -1,41 +1,38 @@
-//comentário de uma linha
+//Comentário de uma linha
 /**
- * * comentário de bloco
+ * Comentário de bloco
  */
 
-//console de saída
-console.log("alô mundo")
+//Console de saída
+console.log("Alô mundo!")
 
-// // //documnet que escreve direto na página
-// // //document.write("alô mundo")
+//Document que escreve direto na página
+//document.write("Alô mundo!")
 
-// // var nome
+//declarar variável de escopo global (var)
+//declarar variável local (let)
+//declarar variável de escopo global que não há alteração (const)
 
-// // function alteraNome(nomeAlt){
-// //     let nome
-// //     this.nome = nomeAlt
-// //     retur nome
-// // }
-
-// // function verification(nome){
-// //     if(nome *** undefined){
-// //         nome * "vazio"
-        
-// //     }
+// function alteraNome(nomeAlt){
+//     let nome
+//     this.nome = nomeAlt
+//     return nome
 // }
 
-//1°passo
-const btn = document.getElementById("btnenviar")
+// function verificacaoNome(nome){
+//     if(nome === undefined){
+//         nome = "vazio"
+//     }
+// }
 
-//atrelar um evento
-//escutar um evento e realizar uma ação
-btn.addEventListener("clik",function(){
+//1oPASSO
+
+const btn = document.getElementById("btnEnviar")
+//Atrelar um evento
+//Escutar um evento e realizar uma ação
+btn.addEventListener("click", ()=>{      //()=> é a mesma coisa que function ArrowFunction
     btn.type = "button"
-    //recuperar o input nmtask querySelector
-    let inputTask = document.querySelectorAll("input[type=text]")
+    //Recuperar o input nmTaks querySelector
+    let inputTask = document.querySelectorAll("input[type=text]") //querySelector
     inputTask[0].value = btn.innerHTML
 })
-
-
-//
-
