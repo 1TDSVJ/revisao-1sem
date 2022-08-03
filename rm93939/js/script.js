5cb03bd8dae408ca0c0984f0be7e386f9880a3f2
Pedrochueiri <pedrochu99@hotmail.com>
2022-08-03 09:33:35 -0300

    add script

---
+++ rm93939/js/script.js
@@ -0,0 +1,10 @@
+//primeiro passo
+const btn = document.getElementById("btnEnviar")
+
+//atrelar um evento
+//escutar um evnto e realizar uma ação
+btn.addEventListener("click",function(){
+    btn.type = "button"
+    //recuperar o input nmTask quertSelector
+    let inputTask = document.querySelector("input[type=text]")
+})
